12deb54b011a71867398915ff696aef585ce04e7
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2025-07-12 20:34:20 +0530

    Update snowflake_connector.py

--- utils/snowflake_connector.py
+++ utils/snowflake_connector.py
@@ -20,7 +20,7 @@ def get_states():
 def get_state_info(state_name):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute(f"SELECT * FROM states_info WHERE state_name = %s", (state_name,))
+    cur.execute("SELECT * FROM states_info WHERE state_name = %s", (state_name,))
     row = cur.fetchone()
     if not row:
         return None
@@ -31,5 +31,11 @@ def get_state_info(state_name):
         "places": row[4].split(','),
         "cuisine": row[5].split(','),
         "festivals": row[6].split(','),
-        "language_snippets": eval(row[7])  # list of dicts
+        "language_snippets": eval(row[7])
     }
+
+def log_visit(state):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO visits (state, timestamp) VALUES (%s, current_timestamp())", (state,))
+    conn.commit()
